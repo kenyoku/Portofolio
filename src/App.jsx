e37e0a4a3defae1deeec07762268506d4c44046d
Yokuken <torunp850409@gmail.com>
2025-06-08 19:03:49 +0900

    Add resource files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import CustomCursor from './components/CustomCursor';
+
+export default function App() {
+
+return (
+<>
+<Header/> 
+<HeroSection/>
+<CustomCursor/>
+</>
+)
+}
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,56 @@
+import {motion } from "framer-motion";
+import Spline from "@splinetool/react-spline";
+
+
+const HeroSection =()=>{
+  return (
+ <section className="h-screen bg-gradient-to-b 
+ from-violet-900 to-black flex xl:flex-row
+ flex-col-reverse items-center justify-between
+ lg:px-24 px-10 relative overflow-hidden">
+    {/*Left Section */}
+    <div className="z-40 xl:mb-0 mb-[20%]">
+        <motion.h1
+        initial={{opacity:0,y:80}}
+        animate={{opacity:1,y:0}}
+        transition={{
+            type:"spring",
+            stiffness:40,
+            damping:25,
+            delay:1.3,
+            duration:1.5,
+        }}
+        className="text-5xl md:text-7xl
+        lg:text-8xl font-bold z-10 mb-6"></motion.h1>
+
+        <motion.p
+         initial={{opacity:0,y:80}}
+         animate={{opacity:1,y:0}}
+         transition={{
+             type:"spring",
+             stiffness:40,
+             damping:25,
+             delay:1.3,
+             duration:1.5,
+         }}
+        className="text-xl md:text-1xl
+        lg:text-2xl text-purple-200 max-w-2xl">
+            I am a GOOD engineer
+        </motion.p>
+    </div>
+
+    {/*Right Section*/}
+    <Spline className="absolute xl:right-[28%]
+    right-0 top-[-20%] lg:top-0"
+    scene="https://prod.spline.design/Pbx-AhdgbGKk2cpq/scene.splinecode" />
+
+ </section>
+  
+
+  )
+
+
+}
+
+
+export default HeroSection
